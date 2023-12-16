8ee560fc078d47d80791aa0eac7a390b8f9c014b
nesliprm <nesliprm@gmail.com>
2023-12-16 22:58:06 +0100

    styled footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,27 +6,28 @@ export default function Footer() {
     <div className="Footer">
       <small>
         <a
-          href="https://github.com/nesliprm/cocktail-generator"
+          href="https://github.com/nesliprm/dictionary-react"
           target="_blank"
+          rel="noreferrer"
           class="link-dark"
         >
           Open-source Code
         </a>{" "}
         by ▼ Nesli Parmaksizoglu
       </small>
-      <div>
-        <small>
-          Hosted on{" "}
-          <a
-            href="https://gilded-lokum-c4a09c.netlify.app/"
-            target="_blank"
-            class="link-dark"
-          >
-            Netlify
-          </a>
-          .
-        </small>
-      </div>
+      <br />
+      <small>
+        Hosted on{" "}
+        <a
+          href="https://gilded-lokum-c4a09c.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          class="link-dark"
+        >
+          Netlify
+        </a>
+        .
+      </small>
     </div>
   );
 }
